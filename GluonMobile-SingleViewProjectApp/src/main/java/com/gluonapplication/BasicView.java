403018846ee30857ef1c77372b693597d955cc54
enieqgn <qing.geng@ericsson.com>
2019-04-19 11:17:21 +0800

    math

--- GluonMobile-SingleViewProjectApp/src/main/java/com/gluonapplication/BasicView.java
+++ GluonMobile-SingleViewProjectApp/src/main/java/com/gluonapplication/BasicView.java
@@ -5,6 +5,8 @@ import com.gluonapplication.math.MathBlockExtractor;
 import com.gluonapplication.math.Type;
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,6 +14,8 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class BasicView extends View {
 
@@ -23,13 +27,15 @@ public class BasicView extends View {
     private MathBlockExtractor mathBlockExtractor;
     private MathBlockExtractor strategAndStepsExtractor;
 
+    private WebEngine engine;
+
 
     public BasicView() {
 
         mathBlockExtractor = new MathBlockExtractor("/Formula.md");
         strategAndStepsExtractor = new MathBlockExtractor("/StrategyAndSteps.md");
         WebView webView = new WebView();
-        WebEngine engine = webView.getEngine();
+        engine = webView.getEngine();
 
 
         engine.loadContent(HtmlTemplate.blockHtmlTemplate, "text/html;charset=utf-8");
@@ -76,6 +82,7 @@ public class BasicView extends View {
 //        appBar.setNavIcon(MaterialDesignIcon.MENU.button(e -> System.out.println("Menu")));
         appBar.setTitleText("MATH");
 //        appBar.getActionItems().add(MaterialDesignIcon.SEARCH.button(e -> System.out.println("Search")));
+        appBar.getActionItems().add(MaterialDesignIcon.WEB_ASSET.button(this::loadFromWeb));
         MenuItem formula = new MenuItem(FORMULA);
         formula.setOnAction(event -> {
                     selectStr = formula.getText();
@@ -89,6 +96,39 @@ public class BasicView extends View {
             appBar.setTitleText(selectStr);
         });
         appBar.getMenuItems().add(strategy);
+    }
+
+    private void loadFromWeb(ActionEvent actionEvent) {
+        engine.load("https://github.com/GengQing/moblie/blob/master/GluonMobile-SingleViewProjectApp/src/main/resources/Formula.md");
+
+        new Thread(() -> {
+            int cnt =0;
+            while (engine.getDocument() == null) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                cnt ++;
+                if (cnt > 20) {
+                    break;
+                }
+            }
+            Document document = engine.getDocument();
+            if (document == null) {
+                System.out.println("the doc is null");
+                return;
+            }
+            NodeList nodeList = document.getElementsByTagName("article");
+            if (nodeList.getLength() == 0) {
+                System.out.println("the article is null");
+            } else {
+                String s = nodeList.item(0).getTextContent();
+                System.out.println(s);
+            }
+
+        }).start();
+
     }
 
 
